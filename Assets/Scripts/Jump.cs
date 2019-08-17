c668e1317a6af4a3f83edf8b0794db77f6cfc094
Jay Shaffer <shaffersan@gmail.com>
2019-08-17 12:20:24 -0600

    Jump mechanics

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    bool onGround = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public bool JumpAllowed(){
+       return onGround; 
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+        if(LayerMask.LayerToName(collider.gameObject.layer) == "Ground"){
+            onGround = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider) {
+       if(LayerMask.LayerToName(collider.gameObject.layer) == "Ground"){
+            onGround = false;
+       }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,18 +6,33 @@ public class PlayerController : MonoBehaviour
 {
     public float cameraDistance = 4f;
     public float speed = 2f;
-    // Start is called before the first frame update
+    public Rigidbody2D rb;
+    public GameObject jumpDetect;
+    public float jumpForce = 5;
+    
     void Start()
     {
-        
+       rb = GetComponent<Rigidbody2D>(); 
+       jumpDetect = GameObject.FindWithTag("jumpdetect");
     }
 
     // Update is called once per frame
     void Update()
     {
        float x = Input.GetAxis("Horizontal"); 
-       float y = Input.GetAxis("Vertical"); 
-       transform.Translate(new Vector3(x, y, 0) * speed * Time.deltaTime);
+       transform.Translate(new Vector3(x, 0, 0) * speed * Time.deltaTime);
+       if(Input.GetButton("Jump") && JumpAllowed()){
+           Jump();
+       }
+    }
+
+    bool JumpAllowed(){
+        Jump jump = jumpDetect.GetComponent<Jump>();
+        return jump.JumpAllowed();
+    }
+
+    void Jump(){
+        rb.velocity = new Vector2 (rb.velocity.x, jumpForce);    
     }
 
     void OnDrawGizmosSelected()
